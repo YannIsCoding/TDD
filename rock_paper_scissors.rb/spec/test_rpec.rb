2f47f450a616c4281fa314d41f03abdc6a03eaf5
Yann Parent <yann.parent4@gmail.com>
2019-10-31 13:28:11 +0100

    Final rps verison with test

---
+++ rock_paper_scissors.rb/spec/test_rpec.rb
@@ -0,0 +1,14 @@
+# require_relative '../test'
+# describe 'capture_name' do
+#   before do
+#     $stdin = StringIO.new("James T. Kirk\n")
+#   end
+
+#   # after do
+#   #   $stdin = STDIN
+#   # end
+
+#   it "should be 'James T. Kirk'" do
+#     expect(capture_name).to be == 'James T. Kirk'
+#   end
+# end
